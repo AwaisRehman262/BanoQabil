183105fc0a2e15bc1ec5ac66d9d7e40c0528a929
AwaisRehman262 <aw2946652@gmail.com>
2023-03-25 12:14:22 +0500

    1st commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let my_name = "Awais"
+let my_age = 14
+let my_object ={physics:58,Biology:49,Chemistry:59,Math:73,urdu:91,islamiat:95}
+let my_boolean = True
+let my_array = [Awais,Rehman,Jutt]
+
+alert(my_name,my_age,my_object,my_boolean,my_array)
