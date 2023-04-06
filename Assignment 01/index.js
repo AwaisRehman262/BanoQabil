9e6120f6ccd401b266f30344535a0b10060faf21
Awais Rehman <126328366+AwaisRehman262@users.noreply.github.com>
2023-04-06 20:11:37 +0500

    Completed Assignment02

---
+++ Assignment 01/index.js
@@ -0,0 +1,8 @@
+const Array_1 = [1,2,3,4,5,6,7,8,9,10]
+
+var total = 0;
+for(let i=0; i < Array_1.length; i++){
+    total += Array_1[i]
+}
+
+console.log(total)
---
+++ Assignment 02/index.js
@@ -0,0 +1,7 @@
+let array01 = [1,2,3,4,5,6,7,8,9,9]
+let array02 = []
+for(let i = 1;i<=array01.length;i++){
+    array02.push(i*2)
+}
+
+console.log(array02)
